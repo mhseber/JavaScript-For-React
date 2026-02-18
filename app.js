283f72271540e10763ea43bb451f16958c624cab
mahamudul hasan seber <sebermh@gmail.com>
2026-02-19 00:36:27 +0600

    JavaScript-For-React

---
+++ app.js
@@ -0,0 +1,49 @@
+// JavaScript Fundamentals 
+
+
+// variable 
+
+// let and const 
+
+// let name = "seber";
+// name = "mh.seber";
+// console.log(name)
+
+// if else
+
+// const num = 40;
+
+// if (num>=60 ){
+//     console.log("yes")
+// }
+// else{
+//     console.log("Nooooo")
+// }
+
+
+
+// const brothers = ["fahim", "tusar", "ratul", "rifat", "tasin", "towhid"]
+
+// brothers.push("mahim")
+// brothers.pop()
+// brothers.unshift("thor")
+// brothers.shift()
+// console.log(brothers.slice(2,3))
+// console.log(brothers.splice(2,3))
+
+// console.log(brothers)
+
+
+
+const brothers = ["fahim", "tusar", "ratul", "rifat", "tasin", "towhid"]
+
+const person = {
+    name: "mh",
+    age: "25",
+    friends: brothers
+}
+
+person.name = "seber"
+
+console.log(person)
+
