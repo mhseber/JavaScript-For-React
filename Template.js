bd2f3e51cb367e7e0373b7172881da64886a2cb5
mahamudul hasan seber <sebermh@gmail.com>
2026-02-19 00:51:51 +0600

    Template String, Arrow Function, Spread Operator

---
+++ Template.js
@@ -0,0 +1,30 @@
+// Template String, Arrow Function, Spread Operator
+
+// const fName = "MH.";
+// const lName = "SEBER"
+
+// console.log(`I am ${fName+lName}`)
+
+
+
+
+// function sum(){
+//     console.log(arguments)
+// }
+
+
+
+
+
+// const sum = (a,b,c)=>{
+//     const add = a + b + c;
+//     return add;
+
+// };
+
+// const result = sum(1,2,3)
+// console.log(result)
+
+
+// const code = [1,23,4,5,6,]
+// console.log(...code)
