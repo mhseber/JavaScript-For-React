3cc2fda5e578ef593b019c5e50f37ac0ef1d3c1a
mahamudul hasan seber <sebermh@gmail.com>
2026-02-19 15:21:40 +0600

    (advanced) truthy, false, Ternary Operator, ShortCut

---
+++ Shortcut.js
@@ -0,0 +1,43 @@
+// (advanced) truthy, false, Ternary Operator, ShortCut
+
+
+// const test = ""
+const test = "seber"
+// const test = 0
+// const test = false
+// const test = []
+
+// if (test){
+//     console.log("Truthy")
+// }
+// else{
+//     console.log("Falsy")
+// }
+
+// test ? console.log("its truthy") :  console.log("its falsy")
+
+
+
+// const num = 50;
+// const result = (num >= 10 && num < 20) ? "true" : "false";
+
+// console.log(result)
+
+
+
+// const isActive = true;
+const isActive = false;
+
+const showUser = () => console.log("show user green")
+
+const hideUser = () => console.log("hide user")
+
+// isActive ? showUser(): hideUser();
+
+// isActive && showUser();
+// isActive || showUser();
+
+let num = "10";
+
+const result = num+"seber";
+console.log(typeof result)
